211b25d30d8c70f0658c667e2dc7d1660a071f97
SandeMiriam <miriamsandeoyuga@gmail.com>
2022-02-12 08:39:26 +0300

    first commit

---
+++ src/main/kotlin/String.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var name = "Miriam Sande"
+    println(name)
+    var age = "20"
+    println(age)
+    var phonenumber = "0111817308"
+    println(phonenumber)
+    var weight = "53kg"
+    println(weight)
+    val isKenyan :Boolean =true
+    println (isKenyan) //output true
+    val isnotKenyan :Boolean = false
+    println(isnotKenyan) //output false
+}
